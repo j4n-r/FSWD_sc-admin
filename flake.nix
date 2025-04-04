f3cbbf5b454b30df3f6994787b38413cbbaf4792
j4n-r <jan.ruegge@gmail.com>
2025-04-04 15:00:44 +0200

    Initial Setup

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Python env";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.python312
+            pkgs.python312Packages.flask
+          ];
+        };
+      }
+    );
+}
