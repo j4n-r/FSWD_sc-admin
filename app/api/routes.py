0d9322486fcc80db03279f35c46ecda672d7076f
j4n-r <jan.ruegge@gmail.com>
2025-06-22 12:04:31 +0200

    add new api routes

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,3 +1,4 @@
+from flask import current_app, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
 from app.db import query_db
@@ -6,11 +7,12 @@ from . import api_bp
 
 
 @api_bp.route("/user/<string:user_id>/conversations")
-# @jwt_required(refresh=True)
-def conversation(user_id):
+@jwt_required()
+def getUserConversations(user_id: str):
     """
     Get all conversations for a user.
     """
+    current_app.logger.debug(request)
     conversations = query_db(
         """
             SELECT * FROM conversations c
@@ -24,3 +26,46 @@ def conversation(user_id):
         return {"message": "No conversations found"}, 404
 
     return {"conversations": [dict(conv) for conv in conversations]}, 200
+
+
+@api_bp.route("/conversation/<string:conv_id>/messages")
+@jwt_required()
+def getMessages(conv_id: str):
+    """
+    Get all conversations for a user.
+    """
+    current_app.logger.debug(request)
+    conversations = query_db(
+        """
+            SELECT * FROM messages
+            WHERE conversation_id = ?
+            """,
+        [conv_id],
+    )
+
+    if not conversations:
+        return {"message": "No conversations found"}, 404
+
+    return {"messages": [dict(conv) for conv in conversations]}, 200
+
+
+@api_bp.route("/conversation/<string:conv_id>")
+@jwt_required()
+def getConversation(conv_id: str):
+    """
+    Get all conversations for a user.
+    """
+    current_app.logger.debug(request)
+    conversation = query_db(
+        """
+            SELECT * FROM conversations
+            WHERE id = ?
+            """,
+        [conv_id],
+        True,
+    )
+
+    if not conversation:
+        return {"message": "Conversation not found"}, 404
+
+    return {"messages": conversation}, 200
